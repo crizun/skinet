1eb5ab3ad80a291c7982d1968418dfedad361fe1
stealthprism <mdanwaar5@outlook.com>
2022-11-19 12:44:06 +0300

    Specificatio patter implemented

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -35,7 +35,8 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _productsRepo.GetByIdAsync(id);
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            var product = await _productsRepo.GetEntityWithSpec(spec);
 
             return Ok(product);
         }
